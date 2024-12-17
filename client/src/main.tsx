4999953f3b31e6647adff43cf3d4ab45899f6c2e
barharush9 <barexg1@gmail.com>
2024-12-17 14:15:48 +0200

    added calaendar page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import {MyProvider,useMyContext} from "./Context.tsx";
-createRoot(document.getElementById("root")!).render(<MyProvider><App /></MyProvider>);
+import { MyProvider, useMyContext } from "./Context.tsx";
+createRoot(document.getElementById("root")!).render(
+  <MyProvider>
+    <App />
+  </MyProvider>
+);
