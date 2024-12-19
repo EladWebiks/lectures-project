c8a3c79ca8bb7df65a11a686635bc6171efcd81b
barharush9 <barexg1@gmail.com>
2024-12-19 10:05:17 +0200

    here we go again

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,17 +2,11 @@ import { FC } from "react";
 import "./HomePage.css";
 import { useMyContext } from "../../Context";
 
-
-
 const HomePage: FC = () => {
-    const { user } = useMyContext();
+  const { user } = useMyContext();
   return (
     <main className="HomePage page">
-<<<<<<< HEAD
       <h1>{user?.username}</h1>
-=======
-      home
->>>>>>> 5bc5325667bf6a66bf6ace9b4d030f2edc3e69e4
     </main>
   );
 };
