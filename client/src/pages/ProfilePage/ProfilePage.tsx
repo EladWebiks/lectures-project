51f67cc133b9c7c0cd04756b42ff126fc7747eeb
ohad roth <ohadroth22@gmail.com>
2024-12-19 09:54:12 +0200

    update from ariel

---
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+const ProfilePage = () => {
+  return (
+    <main>
+        <div className='TopBackgroudnd'>
+
+        </div>
+        <header>
+            <div>
+                <h2>ohad rot</h2>
+                <h4>ohadroth@gmail.com</h4>
+            </div>
+            <div>
+            <Button variant="contained">Edit</Button>
+            </div>
+        </header>
+    </main>
+  )
+}
+
+export default ProfilePage
